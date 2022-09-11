6fc7ab8f63c41527c54a78293ab1b1678452afb4
Tobias Hattinger <tobias.hattinger@gmx.at>
2022-09-11 19:06:01 +0200

    Added extension for wrapSQL

---
+++ ExtTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using RampSQL.Extensions;
+using RampSQL.Query;
+using RampSQL.Reader;
+using RampSQL.Schema;
+using RampSQL.Search;
+using WrapSQL;
+
+namespace ExtTest
+{
+    internal class Program
+    {
+        class RDB : IRampSchema
+        {
+
+            public static RampTable_Data Data = new RampTable_Data();
+            [BindTable("data")]
+            public class RampTable_Data : RampTable
+            {
+                [BindColumn("a", typeof(string))]
+                public RampColumn Name { get; set; }
+                [BindColumn("b", typeof(string))]
+                public RampColumn Description { get; set; }
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            using (WrapMySQL sql = new WrapMySQL("", "", "", ""))
+            {
+                sql.Open();
+                sql.RampNoQuery(new QueryEngine().SelectFrom(""));
+
+                using (RampReader reader = sql.RampQuery(new QueryEngine().SelectFrom("").Where.Is(null, "")))
+                {
+                    while (reader.Read())
+                    {
+                        int i = reader.Get<int>(RDB.Data.Name);
+                    }
+                }
+
+                sql.Close();
+
+                SearchEngine engine = null;
+
+                engine.Connector()
+            }
+
+
+        }
+    }
+}
---
+++ RampSQL-Extensions/WrapSQLConnector.cs
@@ -0,0 +1,28 @@
+﻿using RampSQL.Query;
+using RampSQL.Reader;
+using System.Data;
+using System.Data.Common;
+using WrapSQL;
+
+namespace RampSQL.Extensions
+{
+    public static class WrapSQLConnector
+    {
+        #region WrapSQL extensions
+        public static void RampNoQuery(this WrapSQLBase sql, IQuerySection query) => sql.ExecuteNonQuery(query.ToString(), query.GetParameters());
+        public static void RampNoQueryACon(this WrapSQLBase sql, IQuerySection query) => sql.ExecuteNonQueryACon(query.ToString(), query.GetParameters());
+        public static T RampScalar<T>(this WrapSQLBase sql, IQuerySection query) => sql.ExecuteScalar<T>(query.ToString(), query.GetParameters());
+        public static T RampScalarACon<T>(this WrapSQLBase sql, IQuerySection query) => sql.ExecuteScalarACon<T>(query.ToString(), query.GetParameters());
+        public static object RampScalar(this WrapSQLBase sql, IQuerySection query) => sql.ExecuteScalar(query.ToString(), query.GetParameters());
+        public static object RampScalarACon(this WrapSQLBase sql, IQuerySection query) => sql.ExecuteScalarACon(query.ToString(), query.GetParameters());
+        public static RampReader RampQuery(this WrapSQLBase sql, IQuerySection query) => new RampReader(sql.ExecuteQuery(query.ToString(), query.GetParameters()));
+        public static DataTable RampDataTable(this WrapSQLBase sql, IQuerySection query) => sql.CreateDataTable(query.ToString(), query.GetParameters());
+        public static DataAdapter RampDataAdapter(this WrapSQLBase sql, IQuerySection query) => sql.GetDataAdapter(query.ToString(), query.GetParameters());
+
+        #endregion
+
+        #region RampSQL Search extensions
+
+        #endregion
+    }
+}
